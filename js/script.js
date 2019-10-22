a70bceb11f305a5562fce757196949950b4c2144
isabellawhitfield <53159435+isabellawhitfield@users.noreply.github.com>
2019-10-22 15:09:01 +1300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,92 @@
+var dating = [
+    {
+        name : "Stacey Lane Mathews",
+        gender : "Female",
+        simulation : true,
+        age : 30,
+        photo: "images/chiwhawha.jpeg"
+   
+   
+    },
+   {
+        name : "Kate Tiffany",
+        gender : "Female",
+        simulation : false,
+        age : 25,
+        photo: "images/chiwhawha.jpeg"
+   
+   
+    },
+    {
+        name : "Vincent Peters",
+        gender : "Male",
+        simulation : true,
+        age : 22,
+        photo: "images/chiwhawha.jpeg"
+   
+   
+    },
+    {
+        name : "Friendrich Heinz",
+        gender : "Male",
+        simulation : true,
+        age : 28,
+        photo: "images/chiwhawha.jpeg"
+   
+   
+    },
+    {
+        name : "George Thomas Arthur",
+        gender : "Female",
+        simulation : true,
+        age : 30,
+        photo: "images/chiwhawha.jpeg"
+   
+   
+    },
+    {
+        name : "Stacey Lane Mathews",
+        gender : "Female",
+        simulation : true,
+        age : 30,
+        photo: "images/chiwhawha.jpeg"
+   
+   
+    },
+   {
+        name : "Kate Tiffany",
+        gender : "Female",
+        simulation : false,
+        age : 25,
+        photo: "images/chiwhawha.jpeg"
+   
+   
+    },
+    {
+        name : "Vincent Peters",
+        gender : "Male",
+        simulation : true,
+        age : 22,
+        photo: "images/chiwhawha.jpeg"
+   
+   
+    },
+    {
+        name : "Friendrich Heinz",
+        gender : "Male",
+        simulation : true,
+        age : 28,
+        photo: "images/chiwhawha.jpeg"
+   
+   
+    },
+    {
+        name : "George Thomas Arthur",
+        gender : "Female",
+        simulation : true,
+        age : 30,
+        photo: "images/chiwhawha.jpeg"
+   
+   
+    },
+   ];
