e7ddc694a47afe250c02fd3c88cfae1b78ef332b
isabellawhitfield <isabellawhitfield@gmail.com>
2019-10-22 19:52:55 +1300

    Add buttons

--- js/script.js
+++ js/script.js
@@ -1,137 +1,188 @@
 var dating = [
-    {
-        name : "Stacey Lane Mathews",
-        gender : "Female",
-        simulation : true,
-        age : 30,
-        photo: "images/chiwhawha.jpeg"
-   
-   
-    },
-   {
-        name : "Kate Tiffany",
-        gender : "Female",
-        simulation : false,
-        age : 25,
-        photo: "images/chiwhawha.jpeg"
-   
-   
-    },
-    {
-        name : "Vincent Peters",
-        gender : "Male",
-        simulation : true,
-        age : 22,
-        photo: "images/chiwhawha.jpeg"
-   
-   
-    },
-    {
-        name : "Friendrich Heinz",
-        gender : "Male",
-        simulation : true,
-        age : 28,
-        photo: "images/chiwhawha.jpeg"
-   
-   
-    },
-    {
-        name : "George Thomas Arthur",
-        gender : "Male",
-        simulation : true,
-        age : 30,
-        photo: "images/chiwhawha.jpeg"
-   
-   
-    },
-    {
-        name : "Amelia Charlotte",
-        gender : "Female",
-        simulation : false,
-        age : 24,
-        photo: "images/chiwhawha.jpeg"
-   
-   
-    },
-   {
-        name : "Ava Mia Olivia",
-        gender : "Female",
-        simulation : false,
-        age : 26,
-        photo: "images/chiwhawha.jpeg"
-   
-   
-    },
-    {
-        name : "Aaron Gabriel Baldwin",
-        gender : "Male",
-        simulation : true,
-        age : 20,
-        photo: "images/chiwhawha.jpeg"
-   
-   
-    },
-    {
-        name : "Daniel Felix Barry",
-        gender : "Male",
-        simulation : true,
-        age : 29,
-        photo: "images/chiwhawha.jpeg"
-   
-   
-    },
-    {
-        name : "Harold Earl Fabian",
-        gender : "Male",
-        simulation : false,
-        age : 30,
-        photo: "images/chiwhawha.jpeg"
-   
-   
-    },
-    {
-        name : "Abigail Evelyn Beth",
-        gender : "Female",
-        simulation : false,
-        age : 19,
-        photo: "images/chiwhawha.jpeg"
-   
-   
-    },
-   {
-        name : "Scarlett Emma Grace",
-        gender : "Female",
-        simulation : false,
-        age : 20,
-        photo: "images/chiwhawha.jpeg"
-   
-   
-    },
-    {
-        name : "Ronnie Vin Calvin",
-        gender : "Male",
-        simulation : true,
-        age : 18,
-        photo: "images/chiwhawha.jpeg"
-   
-   
-    },
-    {
-        name : "Caleb Harry Abe",
-        gender : "Male",
-        simulation : true,
-        age : 27,
-        photo: "images/chiwhawha.jpeg"
-   
-   
-    },
-    {
-        name : "Gabe Ned Eddy",
-        gender : "Male",
-        simulation : false,
-        age : 22,
-        photo: "images/chiwhawha.jpeg"
-   
-   
-    },
-   ];
+  {
+    name: "Stacey Lane Mathews",
+    gender: "Female",
+    simulation: true,
+    age: 30,
+    favouriteColour: "Blue",
+
+
+  },
+  {
+    name: "Kate Tiffany",
+    gender: "Female",
+    simulation: false,
+    age: 25,
+    favouriteColour: "Green",
+
+
+  },
+  {
+    name: "Vincent Peters",
+    gender: "Male",
+    simulation: true,
+    age: 22,
+    favouriteColour: "Pink",
+
+
+  },
+  {
+    name: "Friendrich Heinz",
+    gender: "Male",
+    simulation: true,
+    age: 28,
+    favouriteColour: "Purple",
+
+
+  },
+  {
+    name: "George Thomas Arthur",
+    gender: "Male",
+    simulation: true,
+    age: 30,
+    favouriteColour: "Blue",
+
+
+  },
+  {
+    name: "Amelia Charlotte",
+    gender: "Female",
+    simulation: false,
+    age: 24,
+    favouriteColour: "Yellow",
+
+
+  },
+  {
+    name: "Ava Mia Olivia",
+    gender: "Female",
+    simulation: false,
+    age: 26,
+    favouriteColour: "Red",
+
+
+  },
+  {
+    name: "Aaron Gabriel Baldwin",
+    gender: "Male",
+    simulation: true,
+    age: 20,
+    favouriteColour: "Green",
+
+
+  },
+  {
+    name: "Daniel Felix Barry",
+    gender: "Male",
+    simulation: true,
+    age: 29,
+    favouriteColour: "Pink",
+
+
+  },
+  {
+    name: "Harold Earl Fabian",
+    gender: "Male",
+    simulation: false,
+    age: 30,
+    favouriteColour: "Orange",
+
+
+  },
+  {
+    name: "Abigail Evelyn Beth",
+    gender: "Female",
+    simulation: false,
+    age: 19,
+    favouriteColour: "Navy",
+
+
+  },
+  {
+    name: "Scarlett Emma Grace",
+    gender: "Female",
+    simulation: false,
+    age: 20,
+    favouriteColour: "Scarlet",
+
+
+  },
+  {
+    name: "Ronnie Vin Calvin",
+    gender: "Male",
+    simulation: true,
+    age: 18,
+    favouriteColour: "Yellow",
+
+
+  },
+  {
+    name: "Caleb Harry Abe",
+    gender: "Male",
+    simulation: true,
+    age: 27,
+    favouriteColour: "Blue",
+
+
+  },
+  {
+    name: "Gabe Ned Eddy",
+    gender: "Male",
+    simulation: false,
+    age: 22,
+    favouriteColour: "Green",
+
+
+  },
+];
+
+document.getElementById('female').addEventListener('click', function () {
+  document.getElementById('results').innerHTML = "";
+  for (var i = 0; i < dating.length; i++) {
+    if (dating[i].gender === "Female") {
+      document.getElementById('results').innerHTML += getHtml(dating[i]);
+    }
+  }
+});
+
+document.getElementById('male').addEventListener('click', function () {
+  document.getElementById('results').innerHTML = "";
+  for (var i = 0; i < dating.length; i++) {
+    if (dating[i].gender === "Male") {
+      document.getElementById('results').innerHTML += getHtml(dating[i]);
+    }
+  }
+});
+
+document.getElementById('simulation').addEventListener('click', function () {
+  document.getElementById('results').innerHTML = "";
+  for (var i = 0; i < dating.length; i++) {
+    if (dating[i].simulation === true) {
+      document.getElementById('results').innerHTML += getHtml(dating[i]);
+    }
+  }
+});
+
+document.getElementById('blue').addEventListener('click', function () {
+  document.getElementById('results').innerHTML = "";
+  for (var i = 0; i < dating.length; i++) {
+    if (dating[i].favouriteColour === "Blue") {
+      document.getElementById('results').innerHTML += getHtml(dating[i]);
+    }
+  }
+});
+
+document.getElementById('results').innerHTML = "";
+for (var i = 0; i < dating.length; i++) {
+  document.getElementById('results').innerHTML += getHtml(dating[i]);
+}
+
+function getHtml(person) {
+  return '<div class="card-body">' +
+    '<h5 class="card-title">' + person.name + '</h5>' +
+    '<p class="card-text">gender: ' + person.gender + '</p>' +
+    '<p class="card-text">simulation: ' + person.simulation + '</p>' +
+    '<p class="card-text">age: ' + person.age + '</p>' +
+    '<p class="card-text">favouriteColour: ' + person.favouriteColour + '</p>' +
+    '</div>';
+}
